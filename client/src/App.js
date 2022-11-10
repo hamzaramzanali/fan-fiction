f72fdf3a48d0a9f690c1285acad9d4472d86eda0
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-11-10 15:03:52 -0600

    added changes to push to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 // Apollo Provider to make every request work with the Apollo Server
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
@@ -40,6 +40,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <>
+        <Link to ={"/profile"}>Profile</Link>
           <Navbar />
       <Header />
           <Routes>
