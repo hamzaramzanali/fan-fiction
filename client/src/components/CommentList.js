f5ded13fea5d16c30870e5d1116c4c93a246ea31
terrence <terrence.budnik@gmail.com>
2022-11-14 15:07:32 -0600

    comment section

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import '../css/commentsPage.css'
 
+const styles = {
+  author: {
+    visibility: 'hidden'
+  },
+}
 
 const CommentList = ({ comments = [] }) => {
   if (!comments.length) {
-    return <h3 className='commentText'>No Comments Yet</h3>;
+    return <h3 className='commentText'>No Community Contributions Yet</h3>;
   }
 
+  
+  
   return (
     <>
-      <h3 id='thoughtHeader'
+      <h2 id='commentHeader'
         className="p-5 display-inline-block"
         style={{ borderBottom: '1px dotted #880f00' }}
       >
-        Comments
-      </h3>
+        Contributions
+      </h2>
       <div id='thoughtsCard' className="flex-row my-4">
-        {comments &&
-          comments.map((comment) => (
-            <div key={comment._id} className="col-12 mb-3 pb-3">
-              <div className="p-3 text-light">
-                <h5 className="card-header">
-                  {comment.commentAuthor} commented{' '}
-                  <span style={{ fontSize: '0.825rem' }}>
-                    on {comment.createdAt}
-                  </span>
-                </h5>
-                <p className="card-body">{comment.commentText}</p>
-              </div>
-            </div>
+        {comments && comments.map((comment) => (
+          <span key={comment._id}>{comment.commentText} <cite className='commentAuthor' style={styles.author}>{comment.commentAuthor}</cite> </span>
           ))}
       </div>
     </>
@@ -36,3 +32,5 @@ const CommentList = ({ comments = [] }) => {
 };
 
 export default CommentList;
+
+           
